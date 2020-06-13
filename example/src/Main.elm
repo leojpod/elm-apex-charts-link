304033d773238cdb12cc6b636f87563cc43a8703
Leo jPod <leo.jpod@lilo.org>
2020-06-13 20:19:14 +0200

    I'll move this around in a bit

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -1,14 +1,19 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser
-import Html.Attributes exposing (class)
 import Html exposing (Html, div)
+import Html.Attributes exposing (class, id)
+import Json.Encode
 import Platform exposing (Program)
 
 
+type alias Login = 
+  { user: String
+  , date: Time.Posix
+  }
+
 type alias Model =
-    { data1 : ()
-    , data2 : ()
+    { logins : List Login
     }
 
 
@@ -37,7 +42,10 @@ init () =
 
 view : Model -> Html Msg
 view _ =
-    div [ class "container" ] [] []
+    div [ class "container flex flex-col" ]
+        [ div [ id "chart1" ] []
+        , div [ id "chart2" ] []
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -48,3 +56,6 @@ update _ model =
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
+
+
+port updateChart : Json.Encode.Value -> Cmd msg
