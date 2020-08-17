9fcd5ad0eddd970da0a02b1e426bba27637adb48
Leo jPod <leo.jpod@lilo.org>
2020-08-17 22:12:02 +0200

    this should not be versioned΅

---
+++ lib/src/apex-chart-element.ts
@@ -0,0 +1,9 @@
+class ApexChart extends HTMLElement {
+  constructor() {
+    super();
+  }
+  // put the code that reads the chart-data provided by the elm side
+  // -> transform it into a nice chart (possibly with some default)
+}
+
+customElements.define("apex-chart", ApexChart);
