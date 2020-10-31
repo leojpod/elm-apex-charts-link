97acbbad0755638f0c36149c4dda10aee59e616b
Leo jPod <leo.jpod@lilo.org>
2020-10-31 08:55:13 +0100

    set the proper build command

--- example/src/index.js
+++ example/src/index.js
@@ -1,18 +1,18 @@
-import { Elm } from "./Main.elm";
+import { Elm } from './Main.elm'
 // this is a parcel issue it seems (https://github.com/apexcharts/apexcharts.js/issues/1393)
 //   |
 //   V
-import ApexCharts from "apexcharts/dist/apexcharts.common";
-import "@webcomponents/custom-elements";
-import "apex-chart-element";
+import ApexCharts from 'apexcharts/dist/apexcharts.common'
+import '@webcomponents/custom-elements'
+import '../../src/apex-chart-element'
 
-const app = Elm.Main.init({ node: document.getElementById("my-app") });
+const app = Elm.Main.init({ node: document.getElementById('my-app') })
 
 // APEX CHART ZONE
 app.ports.updateChart.subscribe((chartDescription) => {
   const chart = new ApexCharts(
-    document.querySelector("#chart1"),
+    document.querySelector('#chart1'),
     chartDescription
-  );
-  chart.render();
-});
+  )
+  chart.render()
+})
