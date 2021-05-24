7b716f16681d95d67318336de6b16d2fc4356d79
Leo jPod <leo.jpod@lilo.org>
2021-05-24 17:31:42 +0200

    Merge pull my finger request ~ provided by whatthecommit

--- src/Charts/RoundCharts.elm
+++ src/Charts/RoundCharts.elm
@@ -1,11 +1,49 @@
 module Charts.RoundCharts exposing (..)
 
+import Charts.Options exposing (Options)
 
-type alias RoundChartData = 
-  {
-    name : String
-    , series: List (String, Float)
-  }
 
-type RoundChart = RoundChart RoundChartData
+type RoundChartType
+    = Pie
+    | Radial
 
+
+type alias RoundChartOptions =
+    { type_ : RoundChartType }
+
+
+defaultChartOptions : RoundChartOptions
+defaultChartOptions =
+    { type_ = Pie }
+
+
+type alias RoundChartData =
+    { name : String
+    , series : List ( String, Float )
+    , chartOptions : RoundChartOptions
+    , options : Options
+    }
+
+
+type RoundChart
+    = RoundChart RoundChartData
+
+
+pieChart : String -> List ( String, Float ) -> RoundChart
+pieChart name series =
+    RoundChart
+        { name = name
+        , series = series
+        , chartOptions = { defaultChartOptions | type_ = Pie }
+        , options = Charts.Options.default
+        }
+
+
+radialBar : String -> List ( String, Float ) -> RoundChart
+radialBar name series =
+    RoundChart
+        { name = name
+        , series = series
+        , chartOptions = { defaultChartOptions | type_ = Radial }
+        , options = Charts.Options.default
+        }
